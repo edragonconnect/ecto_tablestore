2d5b4c952f4e1d2ef0d72e2a08fbd79023166015
feng19 <feng_19@foxmail.com>
2021-04-20 17:54:25 +0800

    feat: add some DSL call to locals_without_parens settings

--- test/support/migrations/drop/5_drop.exs
+++ test/support/migrations/drop/5_drop.exs
@@ -3,9 +3,9 @@ defmodule EctoTablestore.Repo.Migrations.Drop do
   use EctoTablestore.Migration
 
   def change do
-    drop(table("migration_test1"))
+    drop table("migration_test1")
 
-    drop(secondary_index("migration_test2", "migration_test2_index1"))
-    drop(secondary_index("migration_test2", "migration_test2_index2"))
+    drop secondary_index("migration_test2", "migration_test2_index1")
+    drop secondary_index("migration_test2", "migration_test2_index2")
   end
 end
